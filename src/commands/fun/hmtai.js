c299e57b879a8b5db8fd6a9878132247fc21c6a0
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-02-17 16:25:02 +0100

    Add new fetch system

--- src/commands/fun/hmtai.js
+++ src/commands/fun/hmtai.js
@@ -1,5 +1,6 @@
 const Command = require("../Command.js");
-const hmtai = require("./../../utils/hmtai/index");
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports = class HmtaiCommand extends Command {
   constructor(client) {
@@ -8,70 +9,25 @@ module.exports = class HmtaiCommand extends Command {
       usage: "hm <type> <category>",
       description: "Sends random Hentai image",
       type: client.types.FUN,
-      examples: ["hm sfw neko", "hm nsfw ass", "hm commands"],
+      examples: ["hm neko"],
     });
   }
   async run(message, args) {
     try {
-      switch (args[0]) {
-        case "sfw":
-          message.channel.send(`${hmtai[args[1]]()}`);
-          break;
-        case "nsfw":
-          message.channel.nsfw
-            ? message.channel.send(`${hmtai.nsfw[args[1]]()}`)
-            : message.channel.send(`This is not an NSFW channel`);
-          break;
-        case "commands":
-          message.channel.send(
-            `
-                  **All commands**
-          
-                  SFW:
-                  - wallpaper
-                  - mobileWallpaper
-                  - neko
-                  - jahy
-                  - slap
-                  - lick
-                  - depression
-                  - christmas
-                  - legs
-          
-                  NSFW:
-                  - ass
-                  - bdsm
-                  - cum
-                  - creampie
-                  - manga
-                  - femdom
-                  - hentai
-                  - incest
-                  - ero
-                  - orgy
-                  - elves
-                  - pantsu
-                  - cuckold
-                  - blowjob
-                  - boobjob
-                  - foot
-                  - vagina
-                  - ahegao
-                  - uniform
-                  - gangbang
-                  - gif
-                  - nsfwNeko
-                  - glasses
-                  - tentacles
-                  - thighs
-                  - yuri
-                  - zettaiRyouiki
-                  - nsfwChristmas
-                  - nsfwMobileWallpaper
-                  - publicmasturbation
-                  `
-          );
-          break;
+      const res = await fetch(
+        `https://cdn.dergoogler.com/others/hentai-web/images/${args[0]}.json`
+      );
+      const img = await res.json();
+      const embed = new MessageEmbed()
+        .setTitle(args[0])
+        .setImage(img[Math.floor(Math.random() * img.length)])
+        .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setColor(message.guild.me.displayHexColor);
+      if (message.channel.nsfw) {
+        message.channel.send(embed);
+      } else {
+        message.channel.send(`This is not an NSFW channel`);
       }
     } catch (err) {
       message.client.logger.error(err.stack);
