30b4af280d95fbd7b7ca97f53726189141e1898f
Der_Googler <54764558+DerGoogler@users.noreply.github.com>
2022-03-01 14:04:38 +0100

    Improve botinfo

--- src/commands/info/botinfo.js
+++ src/commands/info/botinfo.js
@@ -1,6 +1,7 @@
 const Command = require("../Command.js");
 const { MessageEmbed } = require("discord.js");
 const pkg = require(__basedir + "/package.json");
+const dc_pkg = require("./../../../node_modules/discord.js/package.json");
 const { owner } = require("../../utils/emojis.json");
 const { oneLine, stripIndent } = require("common-tags");
 
@@ -18,9 +19,9 @@ module.exports = class BotInfoCommand extends Command {
     const botOwner = message.client.users.cache.get(message.client.ownerId);
     const prefix = message.client.db.settings.selectPrefix.pluck().get(message.guild.id);
     const tech = stripIndent`
-      Version     :: ${pkg.version}
-      Library     :: Discord.js v12.3.1
-      Environment :: Node.js v16.13.2
+      Version     :: v${pkg.version}
+      Library     :: Discord.js v${dc_pkg.version}
+      Environment :: Node.js ${process.version}
       Database    :: SQLite
     `;
     const embed = new MessageEmbed()
@@ -34,9 +35,7 @@ module.exports = class BotInfoCommand extends Command {
         "**[Invite Me](https://discordapp.com/oauth2/authorize?client_id=726837711851356242&scope=bot&permissions=403008599) | " +
           "[Repository](https://github.com/Hentai-Web/Discord)**"
       )
-      .setImage(
-        "https://raw.githubusercontent.com/Hentai-Web/Windows/master/build/ic_launcher.png"
-      )
+      .setImage("https://raw.githubusercontent.com/Hentai-Web/Windows/master/build/ic_launcher.png")
       .setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
       .setColor(message.guild.me.displayHexColor);
